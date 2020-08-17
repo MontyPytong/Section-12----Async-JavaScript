85c1fbcad919efb7aa9278090a4db6aab10337ff
CalculatorAcasa <ene.georgec@gmail.com>
2020-08-18 02:26:11 +0300

    first commit

---
+++ callbackHell.js
@@ -0,0 +1,32 @@
+const getTodos = (resource, callback) => {
+
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange',() => {
+        //console.log(request, request.readyState);
+        if(request.readyState === 4 && request.status === 200){
+            const data = JSON.parse(request.responseText)
+            callback(undefined, data);
+        } else if(request.readyState === 4){
+            callback('could not fetch data',undefined);
+        }
+    });
+    
+    request.open('GET', resource);
+    request.send();
+
+};
+
+
+getTodos('todos/luigi.json',(error, data) => {
+    console.log(data);
+    getTodos('todos/mario.json', (error, data) =>{
+        console.log(data);
+        getTodos('todos/shaun.json', (error, data) =>{
+            console.log(data);
+        })
+    })
+});
+
+
+
